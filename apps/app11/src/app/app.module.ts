28c0ac0b6ce1a1d1bfcfb691377ac0de4ed73ef8
Steve Witman <stevewitman@gmail.com>
2020-02-11 07:58:21 -0700

    Added angular material (downgraded to 7.3.2)

--- apps/app11/src/app/app.module.ts
+++ apps/app11/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+ 
 import { AppComponent } from './app.component';
+import { MaterialModule } from '@nx11/material';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		MaterialModule
+	],
   providers: [],
   bootstrap: [AppComponent]
 })
